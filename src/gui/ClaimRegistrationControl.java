4abc446ad73657ad3c7e613d263752ac21a020db
unknown <memesis90@gmail.com>
2013-11-10 18:40:41 +0100

    Claim insert

---
+++ src/gui/ClaimRegistrationControl.java
@@ -0,0 +1,43 @@
+package gui;
+
+import javax.swing.JOptionPane;
+
+public class ClaimRegistrationControl {
+	
+	ClaimRegistrationControl()
+	{
+	
+	}
+	
+	
+	public boolean checkinteger( String imput ) {
+	    try {
+	        Integer.parseInt(imput );
+	        return true;
+	    }
+	    catch( Exception e ) {
+	        return false;
+	    }
+	
+	
+    }
+
+	public void checktype(String text, String text2, String text3, String text4, String text5) {
+		// TODO Auto-generated method stub
+		if( checkinteger(text) && !checkinteger(text2)  && !checkinteger(text3) && !checkinteger(text4) && checkinteger(text5)  )
+		{
+			
+			
+		}
+		else
+			JOptionPane.showMessageDialog(null, "Wrong parameter inserted!!! Try again");
+		
+		
+		
+	}
+
+	
+	
+
+
+}
